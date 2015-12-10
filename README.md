437d49c21d5c21015e926bbd5b65d7ca4ed1b397
Nazar Hussain <nazarhussain@gmail.com>
2015-12-11 04:26:43 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# mocha-test-options
+
+> Extend BDD Interfaces for Mocha to provide test options
+
+In normal scenario you can set some variables/propertiess in `beforeEach` using `this.attribute = 'something'` and later can used it inside test cases. But in a scenario if you need to set some option in test and want to access it in `before` or `beforeEach` then this package will help you out.
+
+### Setup
+
+Then simply run mocha with `--ui mocha-test-options`.
+
+### Example
+
+```js
+
+beforeEach(function(){
+  console.log(this.currentTest.testOptions)
+})
+
+describe('scenario 1', function(){
+  it('test 11', {t1: true}, function(){
+    // Do something
+  });
+})
+
+describe('scenario 2', {sc1: true}, function() {
+
+  it('test 21', {t1: true}, function(){
+    // Do something
+  });
+
+  it('test 22', {t2: true}, function(){
+    // Do something
+  });
+
+  it('test 23', function(){
+    // Do something
+  })
+});
+```
+
+You will get following output of console.
+
+```
+scenario 1
+
+{t1: true}
+  ✓ test 11
+
+scenario 2
+
+{sc1: true, t1: true}
+  ✓ test 21
+
+{sc1: true, t2: true}  
+  ✓ test 22
+
+{sc1: true}
+  ✓ test 23
+
+```
+
+### Notes
+
+- `describe`, `it` will accept a third parameter which you can pass for any options including objects of having functions
+- The third parameter is optional, if you don't pass it will follow standard behavior
+- This also applies to `only` and `skip` as well 
---
+++ index.js
@@ -0,0 +1,100 @@
+var Mocha = require('mocha');
+var Suite = require('mocha/lib/suite');
+var Test = require('mocha/lib/test');
+var escapeRe = require('escape-string-regexp');
+var extend = require('util')._extend;
+
+module.exports = Mocha.interfaces['test-options'] = function(suite) {
+  var suites = [suite];
+
+  suite.on('pre-require', function(context, file, mocha) {
+    var common = require('mocha/lib/interfaces/common')(suites, context);
+
+    context.before = common.before;
+    context.after = common.after;
+    context.beforeEach = common.beforeEach;
+    context.afterEach = common.afterEach;
+    context.run = mocha.options.delay && common.runWithSuite(suite);
+    /**
+     * Describe a "suite" with the given `title`
+     * and callback `fn` containing nested suites
+     * and/or tests.
+     */
+
+    context.describe = context.context = function(title, options, fn) {
+      if(typeof(options) === 'function' && fn == undefined){
+        fn = options;
+        options = {}
+      }
+
+      var suite = Suite.create(suites[0], title);
+      suite.file = file;
+      suite.testOptions = options;
+      suites.unshift(suite);
+      fn.call(suite);
+      suites.shift();
+      return suite;
+    };
+
+    /**
+     * Pending describe.
+     */
+
+    context.xdescribe = context.xcontext = context.describe.skip = function(title, options, fn) {
+      var suite = Suite.create(suites[0], options, title);
+      suite.pending = true;
+      suites.unshift(suite);
+      fn.call(suite);
+      suites.shift();
+    };
+
+    /**
+     * Exclusive suite.
+     */
+
+    context.describe.only = function(title, options, fn) {
+      var suite = context.describe(title, options, fn);
+      mocha.grep(suite.fullTitle());
+      return suite;
+    };
+
+    /**
+     * Describe a specification or test-case
+     * with the given `title` and callback `fn`
+     * acting as a thunk.
+     */
+
+    context.it = context.specify = function(title, options, fn) {
+      var suite = suites[0];
+
+      if(typeof(options) === 'function' && fn == undefined){
+        fn = options;
+        options = {}
+      }
+
+      if (suite.pending) {
+        fn = null;
+      }
+      var test = new Test(title, fn);
+      test.testOptions = extend(extend({}, suite.testOptions), options);
+      test.file = file;
+      suite.addTest(test);
+      return test;
+    };
+
+    context.it.only = function(title, options, fn) {
+      var test = context.it(title, options, fn);
+      var reString = '^' + escapeRe(test.fullTitle()) + '$';
+      mocha.grep(new RegExp(reString));
+      return test;
+    };
+
+    /**
+     * Pending test case.
+     */
+
+    context.xit = context.xspecify = context.it.skip = function(title, options) {
+      context.it(title, options);
+    };
+  });
+};
